23af8dd3c943df4ac20ff32116e7fbca19533675
kishor <kishor@KISHOR-PC>
2019-09-07 13:37:36 +0530

    Add project files.

---
+++ MVCDemoFolderStructure/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemoFolderStructure.Models;
+namespace MVCDemoFolderStructure.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult List()
+        {
+            List<UserModel> userList= new List<UserModel>();
+            var user = new UserModel(1, "kishore", "@yash", "123", "hello", "male", "a1.png", 1);
+            userList.Add(user);
+
+
+            user = new UserModel(1, "ram", "kishore@", "123", "hello", "male", "a2.png", 1);
+            userList.Add(user);
+
+
+
+
+
+            return View(userList);
+        }
+
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            UserModel userModel = new UserModel();
+            userModel.StateId = new StateModel();
+            SelectList countryList = new SelectList(userModel.StateId.GetCountries, "CountryId", "CountryName");
+           
+            SelectList stateList = new SelectList(userModel.GetStates, "StateId", "StateName");
+
+            ViewBag.CountryDataSource = countryList;
+            ViewBag.StateDataSource = stateList;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignUp(UserModel userModel)
+        {
+            userModel.StateId = new StateModel();
+            SelectList countryList = new SelectList(userModel.StateId.GetCountries, "CountryId", "CountryName");
+            SelectList stateList = new SelectList(userModel.GetStates, "StateId", "StateName");
+            ViewBag.CountryId = countryList;
+            ViewBag.StateId = stateList;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult SignIn(UserModel userModel)
+        {
+           TempData["message"] = "sign done !";
+            return RedirectToAction("SignUp");
+        }
+     
+    }
+}
---
+++ MVCDemoFolderStructure/Models/UserModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemoFolderStructure.Models
+{
+    public class UserModel
+    {
+        public UserModel() { }
+
+
+        public UserModel(int id, string name, string email, string nm, string pass, string gender, string url, int sid)
+        {
+
+            this.UserId = id;
+            this.UserName = name;
+            this.UserImageUrl = url;
+            this.Password = pass;
+            this.MobileNumber = nm;
+            this.StateId = new StateModel() { StateId = sid };
+            this.EmailId = email;
+            this.Gender = gender;
+            //this.StateId.CountryId = new CountryModel();
+        }
+
+        [Key]
+        public int UserId { get; set; }
+        [Required(ErrorMessage="Enter user name.")]
+        [Display(Name="User Name*")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage="Enter EmailId.")]
+        [Display(Name ="EmailId*")]
+        [DataType(DataType.EmailAddress)]
+        public string EmailId { get; set; }
+
+        [Required(ErrorMessage = "Enter Password.")]
+        [Display(Name = "Password*")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Enter Confirm Password.")]
+        [Display(Name = "Confirm Password*")]
+        [Compare("Password",ErrorMessage ="Please Enter the correct password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public string MobileNumber { get; set; }
+        public string UserImageUrl { get; set; }
+        public string Gender { get; set; }
+        public StateModel StateId { get; set; }
+        public List<StateModel> GetStates
+        {
+            get
+            {
+                return new List<StateModel>()
+                {
+                    new StateModel()
+                    {
+                        StateId = 1,
+                        StateName = "RAJ",
+                    },
+                    new StateModel()
+                    {
+                        StateId = 2,
+                        StateName = "Guj",
+                    },
+                };
+            }
+        }
+    }
+}
